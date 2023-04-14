4a6882921178e7c4322abe76cb486e376162f7c3
Samy <samynassar996@gmail.com>
2023-04-15 01:59:51 +0200

    Remove typed.js and Implement typeWriter form scratch

--- src/lib/TypeWriter.js
+++ src/lib/TypeWriter.js
@@ -1,25 +1,40 @@
-import Typed from 'typed.js';
+const endOfSentence = /[\.\?\!]\s$/;
+const comma = /\D[\,]\s$/;
+const endOfBlock = /[^\/]\n\n$/;
 
-export default class TypeWriter{
-    constructor(elId, msgArr){
-        this.elId = elId;
-        this.option = {
-            strings: msgArr,
-            typeSpeed: 0,
-            showCursor: true,
-            cursorChar: '|',
-        };
+function writer(target, text, i, interval, resolve) {
+    if (i < text.length) {
+      let currentChar = text.charAt(i);
+      let nextChar = text.charAt(i + 1);
+      let pauseDuration = interval;
+  
+      if (endOfSentence.test(currentChar + nextChar)) { // if current and next characters match end of sentence regex
+        pauseDuration *= 20;
+      } else if (comma.test(currentChar + nextChar)) { // if current and next characters match comma regex
+        pauseDuration *= 20;
+      } else if (endOfBlock.test(currentChar + nextChar)) { // if current and next characters match end of block regex
+        pauseDuration *= 15;
+      }
+  
+      setTimeout(function() {
+        target.innerHTML += currentChar;
+        i++;
+        setTimeout(function() {
+            writer(target, text, i, interval, resolve);
+        }, pauseDuration);
+      }, interval);
+    }else {
+        // Resolve the Promise when the function completes
+        resolve();
     }
+  }
 
-    init(){
-        this.writer = new Typed(this.elId, this.option)
-    }
 
-    start(){
-        this.writer.start
-    }
-
-    stop(){
-        this.writer.stop()
-    }
+// Define a wrapper function that returns a Promise
+export default function typeWriter(target, text, interval) {
+    // Wrap the writer function inside a Promise
+    return new Promise(resolve => {
+        // Call the writer function with the resolve callback to signal completion
+        writer(target, text, 0, interval, resolve);
+    });
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,74 @@
-styleEl = document.getElementById('style-text');
-style = document.getElementById('style-tag');
+
+import preStyles from 'raw-loader!./prestyles.css';
+import typeWriter from './lib/typeWriter';
 
 
-styleEl.addEventListener('input', function() {
-    style.textContent = styleEl.textContent;
+let style, editorEl, workEl, terminalEl, skipAnimationEl, pauseEl, observer;
+let animationSkipped = false, done = false, paused = false;
+let browserPrefix;
+
+
+
+
+// Wait for load to get started.
+document.addEventListener("DOMContentLoaded", function() {
+  getEls();
+  createEventHandlers();
+  startAnimation();
 });
+
+
+const getEls = () => {
+  // We're cheating a bit on styles.
+  let preStyleEl = document.createElement('style');
+  preStyleEl.textContent = preStyles;
+  document.head.insertBefore(preStyleEl, document.getElementsByTagName('style')[0]);
+
+  // El refs
+  style = document.getElementById('style-tag');
+
+  editorEl = document.getElementById('editor');
+
+  workEl = document.getElementById('work');
+
+  terminalEl = document.getElementById('terminal');
+
+}
+
+const createEventHandlers = () => {
+  // Options for the observer (which mutations to observe)
+  const config = {childList: true};
+
+  // Create an observer instance linked to the callback function
+  observer = new MutationObserver((mutationList) => {
+    for (const mutation of mutationList) {
+      if (mutation.type === "childList") {
+        // Update page style.
+        style.textContent = mutation.target.textContent;
+
+        // Ensure we stay scrolled to the bottom.
+        editorEl.scrollTop = editorEl.scrollHeight;
+      }
+    }
+  });
+
+  // Start observing the target node for configured mutations
+  observer.observe(editorEl, config);
+}
+
+
+
+
+const startAnimation = async () => {
+  const interval = 0
+
+  await typeWriter(editor, "text1\n", interval);
+  console.log("here")
+  await typeWriter(editor, "text2", interval);
+
+}
+
+
+
+
 
