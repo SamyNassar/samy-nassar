42f48a09d76d4d1f0d59830fb14e84cbd087ca80
Samy <samynassar996@gmail.com>
2023-04-23 10:38:04 +0200

    Remove unused code

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import getBrowserPrefix from './lib/getBrowserPrefix';
 let styleText = [0, 1, 2, 3].map((i) => require('raw-loader!./resources/styles' + i + '.css').default);
 
 
-let style, editorEl, workEl, contactEl, skipAnimationEl, pauseEl, observer;
-let animationSkipped = false, paused = false;
+let style, editorEl, workEl, contactEl, skipAnimationEl, pauseEl;
 let browserPrefix;
 
 const md2html = marked.parse(workText);
@@ -41,28 +40,11 @@ const getEls = () => {
   editorEl = document.getElementById('editor');
   workEl = document.getElementById('work');
   contactEl = document.getElementById('contact-me');
-  pauseEl = document.getElementById('pause');
   skipAnimationEl = document.getElementById('skip');
 
 }
 
 const createEventHandlers = () => {
-  // // Options for the observer (which mutations to observe)
-  // const config = {childList: true};
-
-  // // Create an observer instance linked to the callback function
-  // observer = new MutationObserver((mutationList) => {
-  //   for (const mutation of mutationList) {
-  //     if (mutation.type === "childList") {
-  //       // Update page style.
-  //       // style.textContent = mutation.target.textContent;
-  //     }
-  //   }
-  // });
-
-  // // Start observing the target node for configured mutations
-  // observer.observe(editorEl, config);
-
   editorEl.addEventListener("input", (e) => {
     style.textContent = editorEl.textContent;
   });
@@ -102,14 +84,12 @@ const startAnimation = async () => {
   }
 
   editorEl.setAttribute("contenteditable", true)
-  pauseEl.style.display = "none";
   skipAnimationEl.style.display = "none"
 
 }
 
 // Skip animation
 function skipAnimation() {
-  animationSkipped = true;
 
   let fullText = styleText.join('\n');
 
