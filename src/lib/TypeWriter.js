ff5749b87f698c427511fda88de3ad8a0478d804
Samy <samynassar996@gmail.com>
2023-04-23 03:41:03 +0200

    Implement skip animation

--- src/lib/TypeWriter.js
+++ src/lib/TypeWriter.js
@@ -4,41 +4,54 @@ const endOfSentence = /[\.\?\!]\s$/;
 const comma = /(?!\w),(?=\s)/;
 const endOfBlock = /[^\/]\n\n$/;
 
+let skipAnimations = false
+
+export const skipAnim = () => {
+  skipAnimations = true;
+}
+
+export const isAnimationSkiped = () => {
+  return skipAnimations;
+}
+
 function writer(target, text, i, interval, style, resolve) {
-    if (i < text.length) {
-      let currentChar = text.charAt(i);
-      let nextChar = text.charAt(i + 1);
-      let pauseDuration = interval;
-  
-      if (endOfSentence.test(currentChar + nextChar)) { // if current and next characters match end of sentence regex
-        pauseDuration *= 100;
-      } else if (comma.test(currentChar + nextChar)) { // if current and next characters match comma regex
-        pauseDuration *= 50;
-      } else if (endOfBlock.test(currentChar + nextChar)) { // if current and next characters match end of block regex
-        pauseDuration *= 100;
-      }
-
-      const finalText = textHandler(target.innerHTML, currentChar)
-
-      if(style){
-        style.textContent += currentChar
-      }
-
-      // Ensure we stay scrolled to the bottom.
-      target.scrollTop = target.scrollHeight;
-  
-      setTimeout(() => {
-        target.innerHTML = finalText;
-        i++;
-        setTimeout(() => {
-            writer(target, text, i, interval, style, resolve);
-        }, pauseDuration);
-      }, interval);
-    }else {
-        // Resolve the Promise when the function completes
-        resolve();
-    }
+  if(skipAnimations){
+    resolve();
   }
+  if (i < text.length && !skipAnimations) {
+    let currentChar = text.charAt(i);
+    let nextChar = text.charAt(i + 1);
+    let pauseDuration = interval;
+
+    if (endOfSentence.test(currentChar + nextChar)) { // if current and next characters match end of sentence regex
+      pauseDuration *= 100;
+    } else if (comma.test(currentChar + nextChar)) { // if current and next characters match comma regex
+      pauseDuration *= 50;
+    } else if (endOfBlock.test(currentChar + nextChar)) { // if current and next characters match end of block regex
+      pauseDuration *= 100;
+    }
+
+    const finalText = textHandler(target.innerHTML, currentChar)
+
+    if(style){
+      style.textContent += currentChar
+    }
+
+    // Ensure we stay scrolled to the bottom.
+    target.scrollTop = target.scrollHeight;
+
+    setTimeout(() => {
+      target.innerHTML = finalText;
+      i++;
+      setTimeout(() => {
+          writer(target, text, i, interval, style, resolve);
+      }, pauseDuration);
+    }, interval);
+  }else {
+      // Resolve the Promise when the function completes
+      resolve();
+  }
+}
 
 
 // Define a wrapper function that returns a Promise
--- src/main.js
+++ src/main.js
@@ -1,27 +1,31 @@
 import workText from '!raw-loader!./resources/work.txt';
 import preStyles from 'raw-loader!./prestyles.css';
 import typeWriter from './lib/typeWriter';
+import { skipAnim, isAnimationSkiped } from './lib/typeWriter';
 import getBrowserPrefix from './lib/getBrowserPrefix';
 let styleText = [0, 1].map((i) => require('raw-loader!./resources/styles' + i + '.css').default);
 
 
-let style, editorEl, workEl, terminalEl, skipAnimationEl, pauseEl, observer;
-let animationSkipped = false, done = false, paused = false;
+let style, editorEl, workEl, skipAnimationEl, pauseEl, observer;
+let animationSkipped = false, paused = false;
 let browserPrefix;
 
+const md2html = marked.parse(workText);
+
 import { marked } from 'marked';
+import textHandler from './lib/textHandler';
 
 
 // Wait for load to get started.
 document.addEventListener("DOMContentLoaded", function() {
   // Put the proper prefix for the styles.
-  const browserPrefix = getBrowserPrefix();
+  browserPrefix = getBrowserPrefix();
     styleText = styleText.map(function(text) {
         return text.replace(/-webkit-/g, browserPrefix);
     });
 
   getEls();
-  // createEventHandlers();
+  createEventHandlers();
   startAnimation();
 });
 
@@ -34,40 +38,72 @@ const getEls = () => {
 
   // El refs
   style = document.getElementById('style-tag');
-
   editorEl = document.getElementById('editor');
-
   workEl = document.getElementById('work');
-
-  terminalEl = document.getElementById('terminal');
+  pauseEl = document.getElementById('pause');
+  skipAnimationEl = document.getElementById('skip');
 
 }
 
 const createEventHandlers = () => {
-  // Options for the observer (which mutations to observe)
-  const config = {childList: true};
+  // // Options for the observer (which mutations to observe)
+  // const config = {childList: true};
 
-  // Create an observer instance linked to the callback function
-  observer = new MutationObserver((mutationList) => {
-    for (const mutation of mutationList) {
-      if (mutation.type === "childList") {
-        // Update page style.
-        // style.textContent = mutation.target.textContent;
-      }
-    }
-  });
+  // // Create an observer instance linked to the callback function
+  // observer = new MutationObserver((mutationList) => {
+  //   for (const mutation of mutationList) {
+  //     if (mutation.type === "childList") {
+  //       // Update page style.
+  //       // style.textContent = mutation.target.textContent;
+  //     }
+  //   }
+  // });
 
-  // Start observing the target node for configured mutations
-  observer.observe(editorEl, config);
+  // // Start observing the target node for configured mutations
+  // observer.observe(editorEl, config);
+
+  skipAnimationEl.addEventListener("click", (e) => {
+    e.preventDefault();
+    skipAnim()
+  })
+  
 }
 
+// Start building the page with animation
 const startAnimation = async () => {
-  const interval = 10
+  const interval = 0
 
   await typeWriter(editorEl, styleText[0], interval, style);
   await typeWriter(workEl, workText, interval);
   await typeWriter(editorEl, styleText[1], interval, style);
-  const md2html = marked.parse(workText);
   workEl.innerHTML = md2html
   workEl.classList.toggle('show')
+
+  if(isAnimationSkiped){
+    skipAnimation()
+  }
+
+  pauseEl.style.display = "none";
+  skipAnimationEl.style.display = "none"
+
 }
+
+// Skip animation
+function skipAnimation() {
+  animationSkipped = true;
+
+  let fullText = styleText.join('\n');
+
+  style.textContent = fullText;
+  style.textContent += "\n* { " + browserPrefix + "transition: 0.25s; } \n";
+
+  let editorHTML = "";
+
+  // Convert txt to html.
+  for(let i = 0; i < fullText.length; i++) {
+     editorHTML = textHandler(editorHTML, fullText[i]);
+  }
+
+  editorEl.innerHTML = editorHTML;
+}
+
